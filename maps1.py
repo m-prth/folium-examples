f1f5a8ece55236a2188bf2a6ec21057fb6e4b1e4
Parth Mistry <imparthmistry@gmail.com>
2019-07-21 00:19:15 +0530

    Add files via upload

---
+++ maps1.py
@@ -0,0 +1,33 @@
+import folium
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_picker(elevation):
+    if (elevation<1000):
+        return 'green'
+    elif (1000<=elevation<3000):
+        return 'orange'
+    else:
+        return 'red'
+
+map=folium.Map([38.58,-99.09],zoom_start=6, tiles="OpenStreetMap")
+
+fgv= folium.FeatureGroup(name="Volcanoes")
+
+for lt,ln,el in zip(lat,lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=6, popup=str(int(el))+"m",
+    fill_color=color_picker(el), color = 'black', fill_opacity=0.7))
+
+fgp= folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Gmaps.html")
